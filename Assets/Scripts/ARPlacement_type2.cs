7cc4fa2c659e05cb38e699373dda9b5dc318f950
htgdokania <dokaniaharsh@gmail.com>
2021-02-03 02:14:02 +0530

    initial Commit

---
+++ Assets/Scripts/ARPlacement_type2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+public class ARPlacement_type2 : MonoBehaviour
+{
+
+    private ARRaycastManager rayManager;
+    private GameObject visual;
+
+    void Start()
+    {
+        //get the components
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+
+        //hide the placement indicator visual
+        visual.SetActive(false);
+    }
+
+    void Update()
+    {
+        //shoot a raycast from centre of the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        //if we hit  an AR plane, update position and rotation
+        if(hits.Count > 0 )
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if(!visual.activeInHierarchy)
+                visual.SetActive(true);
+        }
+
+    }
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objecttospawn;
+    private ARPlacement_type2 placementIndicator;
+    
+    void start()
+    {
+        placementIndicator = FindObjectOfType<ARPlacement_type2>();
+    }
+    void update()
+    {
+        if(Input.touchCount >0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(objecttospawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+        }
+    }
+
+}
